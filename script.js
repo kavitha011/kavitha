6141eae285c1950f6fbf4c32b8875964bf71fbb7
kavitha <kavithathangan006@gmail.com>
2025-05-17 10:32:21 +0530

    day 3

---
+++ script.js
@@ -0,0 +1,27 @@
+var a=10;
+var b=30;
+
+console.log(a)
+console.log(b)
+
+console.log(a+b)
+console.log(a-b)
+console.log(a*b)
+console.log(a/b)
+console.log(a%b)
+
+
+function add(a, b){
+    // var a=10;
+    // var b=3;
+    console.log(a+b)
+}
+add()
+function add(a, b){
+     
+    console.log(a+b)
+
+}
+add(20, 30)
+add(50, 30)
+add(10, 30)
