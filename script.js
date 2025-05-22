03447bf12657e710c2b87728174d15025c06ae86
kavitha <kavithathangan006@gmail.com>
2025-05-22 10:37:26 +0530

    day6

--- script.js
+++ script.js
@@ -123,4 +123,53 @@ n=10
 var result = (n%2===0)? "even" : "odd"
 console.log(result)
 
+//Array
+var arr = [10, 20, 30, 40, 50, 60]
 
+for( var index in arr){
+    console.log(index)
+}
+for( var key of arr){
+    console.log(key)
+}
+
+//Object
+var obj = {name : "kavitha", dept : "maths"}
+
+for( var key in obj){
+    console.log(key)
+    console.log(obj [key])
+}
+
+//Spread Operator
+var arr =[1, 2, 3, 4, 5]
+
+var arr2 =[...arr, 6, 7, 8]
+console.log(arr2)
+
+//Destructuring Operator
+var marks = [90, 99, 97, 98, "fail"]
+
+var [m1,m2,m3,m4,m5] = marks;
+console.log(m1)
+console.log(m2)
+console.log(m3)
+console.log(m4)
+console.log(m5)
+
+//callback function
+function sayBye(){
+    console.log("bye")
+}
+function demo(name, callback){
+    console.log("hello " + name)
+    callback()
+}
+demo("kavitha", sayBye);
+
+const btn=document.getElementById("btn");
+
+btn.onclick = function(){
+    window.open("https://www.instagram.com")
+    alert("welcome to instagram")
+}
